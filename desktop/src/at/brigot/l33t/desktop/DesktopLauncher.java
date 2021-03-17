08728fe61421e33fddc452686511d2373f37cf1d
Shadow17402 <brinar84@gmail.com>
2021-03-17 11:38:38 +0100

    libGDX Setup
    Setup as Desktop Application

---
+++ desktop/src/at/brigot/l33t/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package at.brigot.l33t.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import at.brigot.l33t.L33TFrame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new L33TFrame(), config);
+	}
+}
