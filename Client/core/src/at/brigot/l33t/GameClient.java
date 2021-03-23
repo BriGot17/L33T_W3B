010dade6e39ed3736bbbac8b8ce7dcec62292f8b
Shadow17402 <brinar83@gmail.com>
2021-03-23 11:32:52 +0100

    Played with UI a bit

--- Client/core/src/at/brigot/l33t/GameClient.java
+++ Client/core/src/at/brigot/l33t/GameClient.java
@@ -2,32 +2,83 @@ package at.brigot.l33t;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GameClient extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private Stage stage;
+	private Table table;
+	private SpriteBatch batch;
+	private Skin skin;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+
+		skin = new Skin();
+
+		Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+		pixmap.setColor(Color.WHITE);
+		pixmap.fill();
+		skin.add("white", new Texture(pixmap));
+
+		skin.add("default", new BitmapFont());
+
+		TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+		textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+		textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+		textButtonStyle.font = skin.getFont("default");
+		skin.add("default", textButtonStyle);
+
+		Table table = new Table();
+		table.setFillParent(true);
+		stage.addActor(table);
+		
+		final TextButton button = new TextButton("Click me!", skin);
+		table.add(button);
+
+		button.addListener(new ChangeListener() {
+			public void changed (ChangeEvent event, Actor actor) {
+				System.out.println("Clicked! Is checked: " + button.isChecked());
+				button.setText("Good job!");
+			}
+		});
+
+		table.add(new Image(skin.newDrawable("white", Color.RED))).size(64);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		ScreenUtils.clear(0.2f,0.2f,0.2f,1);
+		stage.act(Math.min(Gdx.graphics.getDeltaTime(),1/30f));
+		stage.draw();
 	}
-	
+
+	public void resize (int width, int height) {
+		stage.getViewport().update(width, height, true);
+	}
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		stage.dispose();
+		skin.dispose();
 	}
 }
