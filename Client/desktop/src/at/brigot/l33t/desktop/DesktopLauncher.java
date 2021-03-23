88bb8703ebcda587aa44a9b090e8102aa087e4b8
Shadow17402 <brinar83@gmail.com>
2021-03-23 11:00:21 +0100

    Again added the libGDX Setup in Client
    Dont do any shit there

---
+++ Client/desktop/src/at/brigot/l33t/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package at.brigot.l33t.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import at.brigot.l33t.GameClient;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClient(), config);
+	}
+}
